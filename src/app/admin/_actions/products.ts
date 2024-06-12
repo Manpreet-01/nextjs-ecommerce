c4e4e108512d6e14926ced383cc62517a99ab861
Manpreet Singh <manpreetmss400@gmail.com>
2024-06-12 11:45:05 +0530

    edit action working

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -47,14 +47,65 @@ export async function addProduct(prevState: unknown, formData: FormData) {
     redirect("/admin/products");
 }
 
+const editSchema = addSchema.extend({
+    file: fileSchema.optional(),
+    image: imageSchema.optional()
+});
+
+export async function updateProduct(id: string, prevState: unknown, formData: FormData) {
+    console.log("formData::  ", formData);
+    const results = editSchema.safeParse(Object.fromEntries(formData.entries()));
+
+    if (results.success === false) {
+        return results.error.formErrors.fieldErrors;
+    }
+
+    const data = results.data;
+    const product = await db.product.findUnique({ where: { id } });
+    if (!product) return notFound();
+
+    let filePath = product.filePath;
+
+    if (data.file && data.file.size > 0) {
+        await fs.unlink(product.filePath);
+
+        await fs.mkdir("products", { recursive: true });
+        filePath = `products/${crypto.randomUUID()}${data.file.name}`;
+        await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+    }
+
+    let imagePath = product.imagePath;
+
+    if (data.image && data.image.size > 0) {
+        await fs.unlink("public" + product.imagePath);
+
+        await fs.mkdir("public/images", { recursive: true });
+        imagePath = `/images/${crypto.randomUUID()}${data.image.name}`;
+        await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()));
+    }
+
+    await db.product.update({
+        where: { id },
+        data: {
+            name: data.name,
+            description: data.description,
+            priceInCents: data.priceInCents,
+            filePath,
+            imagePath,
+        }
+    });
+
+    redirect("/admin/products");
+}
+
 export async function toggleProductAvailability(id: string, isAvailableForPurchase: boolean) {
     await db.product.update({ where: { id }, data: { isAvailableForPurchase } });
 }
 
 export async function deleteProduct(id: string) {
     const product = await db.product.delete({ where: { id } });
-    if(!product) return notFound();
+    if (!product) return notFound();
 
-    await fs.unlink(product.filePath)
-    await fs.unlink(`public${product.imagePath}`)
+    await fs.unlink(product.filePath);
+    await fs.unlink(`public${product.imagePath}`);
 }
--- src/app/admin/_components/ProductForm.tsx
+++ src/app/admin/_components/ProductForm.tsx
@@ -5,18 +5,26 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { formatCurrency } from '@/db/formatters';
 import React, { useState } from 'react';
-import { addProduct } from '../_actions/products';
+import { addProduct, updateProduct } from '../_actions/products';
 import { useFormState, useFormStatus } from 'react-dom';
+import { Product } from '@prisma/client';
+import Image from 'next/image';
 
-export default function ProductForm() {
-    const [priceInCents, setPriceInCents] = useState<number | undefined>(0);
-    const [error, action] = useFormState(addProduct, {});
+export default function ProductForm({ product }: { product?: Product | null; }) {
+    const [priceInCents, setPriceInCents] = useState<number | undefined>(product?.priceInCents);
+    const [error, action] = useFormState(product == null ? addProduct : updateProduct.bind(null, product.id), {});
 
     return (
         <form action={action}>
             <div className='space-y-2'>
                 <Label htmlFor="name">Name</Label>
-                <Input type="text" id="name" name='name' required />
+                <Input
+                    type="text"
+                    id="name"
+                    name='name'
+                    defaultValue={product?.name}
+                    required
+                />
                 {error.name && <div className='text-destructive'>{error.name}</div>}
             </div>
             <div className='space-y-2'>
@@ -34,17 +42,21 @@ export default function ProductForm() {
             <div className="text-muted-foreground">{formatCurrency((priceInCents || 0) / 100)}</div>
             <div className='space-y-2'>
                 <Label htmlFor="description">Description</Label>
-                <Textarea id="description" name='description' required />
+                <Textarea id="description" name='description' defaultValue={product?.description} required />
                 {error.description && <div className='text-destructive'>{error.description}</div>}
             </div>
             <div className='space-y-2'>
                 <Label htmlFor="file">File</Label>
-                <Input type='file' id="file" name='file' required />
+                <Input type='file' id="file" name='file' required={product == null} />
+                {product != null && <div className='text-muted-foreground'>{product.filePath}</div>}
                 {error.file && <div className='text-destructive'>{error.file}</div>}
             </div>
             <div className='space-y-2'>
                 <Label htmlFor="image">Image</Label>
-                <Input type='file' id="image" name='image' required />
+                <Input type='file' id="image" name='image' required={product == null} />
+                {product != null &&
+                    <Image src={product.imagePath} alt='product-img' className='text-muted-foreground' width={240} height={240} />
+                }
                 {error.image && <div className='text-destructive'>{error.image}</div>}
             </div>
 
